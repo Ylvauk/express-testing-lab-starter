6bd3ad966974cf68567b3933fe316e81e0738d47
kchia <kchia87@gmail.com>
2019-09-26 14:01:02 -0400

    add starter code

---
+++ db/seed.js
@@ -0,0 +1,10 @@
+const Gif = require("./schema");
+const seedData = require("./seeds.json");
+
+Gif.remove({})
+  .then(() => {
+    return Gif.collection.insert(seedData);
+  })
+  .then(() => {
+    process.exit();
+  });
